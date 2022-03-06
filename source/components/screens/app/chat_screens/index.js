99a652f77836c6bcf6c6fa763b523792373f8862
Shaharukh khan <Shaharukhskb@gmail.com>
2022-03-06 15:24:26 +0530

    add cards

--- source/components/screens/app/chat_screens/index.js
+++ source/components/screens/app/chat_screens/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text,Dimensions,TextInput,StyleSheet} from 'react-native';
+import {View, Text,Dimensions,TextInput,StyleSheet,TouchableOpacity} from 'react-native';
 import {DefaultColours} from '@constants';
 import {Loader} from '@global_components';
 const { width, height } = Dimensions.get("window");
@@ -28,21 +28,52 @@ const ChatScreen = ({navigation}) => {
                alignItems:'center',justifyContent:'center',flexDirection:'row'}}>
               <Text style={{paddingHorizontal:12,fontSize:width*0.06,fontWeight:'bold'}}>Debit/Credit Cards</Text>
             </View>
-            <View style={{height:300,width:400,backgroundColor:"#FFFFFF",justifyContent:'center',alignItems:'center',alignSelf:'center',paddingTop:width*0.2}}>
+            <View style={{paddingTop:width*0.05}}>
+            <View style={{height:300,width:370,backgroundColor:"#FFFFFF",justifyContent:'center',
+            alignItems:'center',alignSelf:'center',paddingBottom:width*0.06,borderRadius:10}}>
             <View style={styles.topField}>
          <TextInput
          style={{fontSize:20,borderBottomWidth:1}}
-         placeholder={'Email Id'}
+         placeholder={'1234-5678-9012-5456'}
+         placeholderTextColor={"#000000"}
+         keyboardType="numeric"
+         />
+        </View>
+        <View style={{flexDirection:'row',paddingTop:width*0.04,width:"80%"}}>
+        <View style={{width:'40%'}}>
+         <TextInput
+         style={{fontSize:20,borderBottomWidth:2,paddingHorizontal:width*0.05}}
+         placeholder={'MM/YY'}
+         placeholderTextColor={"#000000"}
+         keyboardType='name-phone-pad'
+         />
+      </View>
+      <View style={{width:"60%",paddingLeft:width*0.1}} >
+         <TextInput
+         style={{fontSize:20,borderBottomWidth:2,paddingHorizontal:width*0.1}}
+         placeholder={'CVV'}
+         placeholderTextColor={"#000000"}
+         keyboardType="phone-pad"
+         />
+         </View>
+         
+         </View>
+         <View style={styles.topField}>
+         <TextInput
+         style={{fontSize:20,borderBottomWidth:1}}
+         placeholder={'Name'}
          placeholderTextColor={"#000000"}
          />
         </View>
-        <View style={styles.bottomField}>
-         <TextInput
-         style={{fontSize:20,borderBottomWidth:1}}
-         placeholder={'Last Name'}
-         placeholderTextColor={"#000000"}
-         
-         />
+        </View>
+        <View style={{paddingTop:width*0.09}}>
+          <TouchableOpacity onPress={()=>alert('Added Sucessfully')}>
+        <View style={{borderWidth:1,borderRadius:5,paddingVertical:width*0.05,marginHorizontal:width*0.06,backgroundColor:"#5A429B",borderColor:"#5A429B"}}>
+          <Text style={{textAlign:'center',fontSize:width*0.05,color:"#FFFFFF"}}>
+            ADD CARDS
+          </Text>
+        </View>
+        </TouchableOpacity>
         </View>
             </View>
             </View>
@@ -64,7 +95,7 @@ bottomField: {
   marginTop: 10,
   width:'40%',
   borderBottomWidth:1,
-  alignSelf:'center',
+  alignSelf:'center'
   
   
 },
